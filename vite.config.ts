ce898fee8478dccf0fb356d94c9ed64550040b9e
Richard Unterberg <richard.unterberg@lipsia.digital>
2026-06-27 09:24:09 +0200

    basic setup

---
+++ vite.config.ts
@@ -0,0 +1,46 @@
+import path, { dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
+// import tailwindcss from '@tailwindcss/vite'
+// import { viteBeastiesOutput } from '@unterberg/vite-beasties-output'
+import react from '@vitejs/plugin-react'
+// import vike from 'vike/plugin'
+import { defineConfig } from 'vite'
+import tsConf from './tsconfig.json' with { type: 'json' }
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+export const pathAliases = Object.entries(tsConf.compilerOptions.paths).map(([key, [value]]) => {
+  if (key.includes('*')) {
+    const find = new RegExp(`^${key.replace('/*', '/(.*)$')}`)
+    const replacement = path.resolve(__dirname, value.replace('/*', '/$1'))
+    return { find, replacement }
+  }
+  return { find: key, replacement: path.resolve(__dirname, value) }
+})
+
+const pagesBasePath = process.env.PAGES_BASE_PATH?.replace(/\/$/, '')
+const base = pagesBasePath ? `${pagesBasePath}/` : '/'
+
+export default defineConfig({
+  base,
+  plugins: [
+    // vike(),
+    react(),
+    // tailwindcss(),
+    // viteBeastiesOutput({
+    //   outputDirectory: 'dist/client',
+    //   beastiesOptions: {
+    //     allowRules: [
+    //       /data-theme=.*dark/,
+    //       /data-theme=.*light/,
+    //       /^:root:has\(input\.theme-controller/,
+    //       /^:where\(:root\)$/,
+    //     ],
+    //   },
+    // }),
+  ],
+  resolve: {
+    alias: pathAliases,
+  },
+})
